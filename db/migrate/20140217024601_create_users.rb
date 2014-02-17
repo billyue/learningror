6fed9a9f32b28c6547301478a92b07258605b427
Le Yu <ley@twitter.com>
2014-02-17 09:43:23 -0800

    Add all files

--- db/migrate/20140217024601_create_users.rb
+++ db/migrate/20140217024601_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+  def self.up
     create_table :users do |t|
-
+      t.integer :id
+      t.string  :first_name
+      t.string  :last_name
       t.timestamps
     end
+  end
+
+  def self.down
+    drop_table :users
   end
 end
