58277ebe4ed3f9e033852e851e996ebdfe9a3d17
Le Yu <ley@twitter.com>
2014-02-16 18:29:14 -0800

    Changed

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,21 +1,12 @@
-class State < ActiveRecord::Base
-  @states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California",
-	"Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
-	"Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
-	"Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
-	"Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri",
-	"Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey",
-	"New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
-	"Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
-	"South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
-	"Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
-   def self.filter(substring)
-       result = []
-       for state in @states do
-           if state.downcase.include? substring.downcase then 
-                result << state
-           end
-       end
-       return result
-   end
+class State
+  def State.filter(str)
+    states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+    filtered = Array.new
+    states.each do |state|
+      if (state =~ /#{str}/i)
+        filtered.push(state)
+      end
+    end
+    return filtered
+  end
 end
