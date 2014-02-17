99d427dfe8a3fa16c994d9e83b9ed30626637272
Le Yu <ley@twitter.com>
2014-02-17 14:56:59 -0800

    Add files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217172952) do
+ActiveRecord::Schema.define(version: 20140217194729) do
 
   create_table "comments", force: true do |t|
     t.integer  "photo_id"
